c595637dafa15448e0cb1b84047f5e061e8b1037
Derek Threeton <derek3ton@hotmail.com>
2021-02-10 11:38:07 -0800

    Acme Assignment
    Added assignment

---
+++ Acme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+
+
+    class Demo
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \n" );
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+
+}
