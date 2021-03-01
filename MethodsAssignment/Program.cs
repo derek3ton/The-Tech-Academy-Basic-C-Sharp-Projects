26cabacd112f87abb85d49d7c0ef1d85d7b25535
Derek Threeton <derek3ton@hotmail.com>
2021-03-01 15:40:59 -0800

    Add files via upload

---
+++ MethodsAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number. ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Math math = new Math();
+            math.Add = userInput;
+            math.Sub = userInput;
+            math.Mult = userInput;
+
+            int sum = math.Addition(5);
+            Console.WriteLine("The sum of " + userInput + " + 5" + " is:  " + sum);
+
+            int diff = math.Subtraction(5);
+            Console.WriteLine("The difference of " + userInput + " - 5 is: " + diff);
+
+            int prod = math.Multiplication(5);
+            Console.WriteLine("The product of " + userInput + " * 5 is: " + prod);
+
+
+
+            Console.ReadLine();
+        }
+        
+
+    }
+}
+
---
+++ MethodsAssignment/math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Math
+    {
+
+        public int Add { get; set; }
+        public int Sub { get; set; }
+        public int Mult { get; set; }
+
+        public int Addition(int num1)
+        {
+            return num1 + Add;
+        }
+
+        public int Subtraction(int num2)
+        {
+            return num2 - Sub;
+        }
+       
+        public int Multiplication(int num3)
+        {
+            return num3 * Mult;
+        }
+    }
+     
+}
