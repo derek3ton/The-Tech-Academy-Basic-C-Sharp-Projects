25881517fd3173e45057adb12f95b10a27d19e00
Derek Threeton <derek3ton@hotmail.com>
2021-03-02 15:38:26 -0800

    Add files via upload

---
+++ AssignmentPg226/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number. ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Math math = new Math();
+            math.Add = userInput;
+            math.Mult = userInput;
+            math.Div = "userInput";
+
+            int sum = math.Addition(10);
+            Console.WriteLine("The sum of " + userInput + " + 10 is: " + sum);
+
+            int prod = math.Multiply(10);
+            Console.WriteLine("The product of " + userInput + " * 10 is: " + prod);
+  
+
+            
+            int quot = math.Divide("10");
+            Console.WriteLine("The quotient of " + userInput + " / 10 is: " + quot);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AssignmentPg226/math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg226
+{
+    public class Math
+    {
+        public int Add { get; set; }
+        public decimal Mult { get; set; }
+        public string Div { get; set; }
+
+
+        public int Addition(int num1)
+        {
+            return num1 + Add;
+        }
+
+        public int Multiply(decimal num2)
+        {
+            int prod = Convert.ToInt32(num2 * Mult);
+            return prod;
+        }
+
+        public int Divide(string num3)
+        {
+            int div1 = Convert.ToInt32(Div);
+            int div2 = Convert.ToInt32(num3);
+            int quot = Convert.ToInt32(div1 / div2);
+            return quot;
+        }
+
+    }
+}
