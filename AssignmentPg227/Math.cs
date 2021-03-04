fe11432b6745a886c8ec99883e5d3d2251a9fd1a
Derek Threeton <derek3ton@hotmail.com>
2021-03-03 17:24:54 -0800

    Add files via upload

---
+++ AssignmentPg227/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg227
+{
+    public class Math
+    {
+        
+        public int Calculate(int num1, int num2 = 4)
+        {
+            return num1 + num2;
+        }
+        
+        
+
+
+    }
+}
