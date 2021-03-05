2ed8ae5b14006a938f8c1eeca6069b6abd72d835
Derek Threeton <derek3ton@hotmail.com>
2021-03-05 14:16:15 -0800

    Add files via upload

---
+++ AssignmentPg281/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg281
+{
+    public class Math
+    {
+        
+
+        public void Output(int num1, int num2)
+        {
+            int result = num1 + 10;
+            Console.WriteLine(num1 + " + 10 is: " + result);
+
+            Console.WriteLine("The second number you entered is: " + num2);
+        }
+
+    }
+}
---
+++ AssignmentPg281/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); //Instantiates 
+
+            Console.WriteLine("Please enter a whole number."); //Takes integer in
+            int Number1 = Convert.ToInt32(Console.ReadLine()); // and stores in variable
+            
+            Console.WriteLine("Please enter a second whole number."); // Takes in second integer
+            int Number2 = Convert.ToInt32(Console.ReadLine());   // and stores in variable
+
+            math.Output(Number1, Number2); // Calls method and outputs result
+
+
+            Console.ReadLine();
+        }
+    }
+}
