9e0f27d50bab6f6403c68c2eab3ae143449d468b
Derek Threeton <derek3ton@hotmail.com>
2021-03-08 14:13:20 -0800

    Add files via upload

---
+++ AssignmentPg285/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Div div = new Div(); //Instantiates Class "Div"
+
+            Console.WriteLine("Please enter a number to divide by 2."); // Takes in user input 
+            int Number1 = Convert.ToInt32(Console.ReadLine());  // Stores user input into variable
+
+            div.Division(Number1); //Calls method and displays result
+
+            
+            div.Output(out int x); // Calling output parameter from method
+
+            
+            div.Output(out string y);  // Calling and outputing from overloaded method
+
+            Console.WriteLine("Please enter a number of your choosing."); //Getting user input to be used in static method
+            int z = Convert.ToInt32(Console.ReadLine()); //Storing user input into variable
+            Multiply.Multiplication(z);  //Calling from static class / method and outputting result
+
+            Console.ReadLine();
+        }
+    }
+}
