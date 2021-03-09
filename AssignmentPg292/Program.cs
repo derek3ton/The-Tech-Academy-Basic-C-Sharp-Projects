622cb9302ccdcd92494c1073cd8472e485d60ae1
Derek Threeton <derek3ton@hotmail.com>
2021-03-09 10:19:04 -0800

    Add files via upload

---
+++ AssignmentPg292/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AssignmentPg292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(); //Instantiates person class
+
+            Employee employee = new Employee();  //instantiates employee class
+            employee.FirstName = "Sample"; //Initializes Employee object
+            employee.LastName = "Student"; //Initializes employee object
+
+            employee.SayName();  //Calls superclass "Person" method on Employee object
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+ 
