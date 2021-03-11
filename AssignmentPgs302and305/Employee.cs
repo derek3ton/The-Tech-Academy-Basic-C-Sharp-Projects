bca513dc9dbeb55a98851c179d90eec9e5025e3e
Derek Threeton <derek3ton@hotmail.com>
2021-03-10 18:00:30 -0800

    Add files via upload

--- AssignmentPgs302and305/Employee.cs
+++ AssignmentPgs302and305/Employee.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace AssignmentPgs302and305
 {
-    public class Employee : Person, IQuittable  //Implementing Employee class and inheriting from class Person and IQuittable
+    public class Employee<T> : Person //IQuittable  //Implementing Employee class and inheriting from class Person and IQuittable
     {
         public int Id { get; set; }
+        public List<T> Things { get; set; }  //creating generic list property
 
+        //public void Quit()  // Creating method 
+        //{
+        //    Console.WriteLine("I quit."); //Outputs result of calling IQuittable
+        //}
 
-        public void Quit()  // Creating method 
-        {
-            Console.WriteLine("I quit."); //Outputs result of calling IQuittable
-        }
-
-        public static bool operator== (Employee employee, Employee employee1)
-        {
-            return employee.Id == employee1.Id;
-        }
-        public static bool operator !=(Employee employee, Employee employee1)
-        {
-            return employee.Id != employee1.Id;
-        }
+        //public static bool operator== (Employee employee, Employee employee1)
+        //{
+        //    return employee.Id == employee1.Id;
+        //}
+        //public static bool operator !=(Employee employee, Employee employee1)
+        //{
+        //    return employee.Id != employee1.Id;
+        //}
     }
 }
--- AssignmentPgs302and305/Program.cs
+++ AssignmentPgs302and305/Program.cs
@@ -9,24 +9,40 @@ namespace AssignmentPgs302and305
     class Program
     {
         static void Main(string[] args)
-        {   
-            Employee employee = new Employee(); //Instantiating employee class 
-            employee.firstName = "Sample";  //Initializing employee class and setting variable
-            employee.lastName = "Student";
-            
-            employee.SayName();  //Calling method and displaying result. 
+        {
+            //Employee employee = new Employee(); //Instantiating employee class 
+            //employee.firstName = "Sample";  //Initializing employee class and setting variable
+            //employee.lastName = "Student";
 
-            IQuittable quittable = new Employee(); //Instantiating new IQuittable object from Employee class
-            quittable.Quit();  //Calling Method displaying result
+            //employee.SayName();  //Calling method and displaying result. 
+
+            //IQuittable quittable = new Employee(); //Instantiating new IQuittable object from Employee class
+            //quittable.Quit();  //Calling Method displaying result
 
 
+            //employee.Id = 21; //Assign value to employee property Id 
+            //Employee employee1 = new Employee(); //Instantiating employee1 object
+            //employee1.Id = 32; //Assigns value to employee1 property Id
 
-            employee.Id = 21; //Assign value to employee property Id 
-            Employee employee1 = new Employee(); //Instantiating employee1 object
-            employee1.Id = 32; //Assigns value to employee1 property Id
+            //Console.WriteLine(employee1 == employee); //Compares the two objects and returns false
+            //Console.WriteLine(employee1 != employee); //Compares the two objects and returns true
 
-            Console.WriteLine(employee1 == employee); //Compares the two objects and returns false
-            Console.WriteLine(employee1 != employee); //Compares the two objects and returns true
+            Employee<string> list1 = new Employee<string>();  //Instantiates 
+            list1.Things = new List<string>();  //Creates new list
+            list1.Things.Add("Hammer");
+            list1.Things.Add("Basketball");
+            list1.Things.Add("Balloon");
+
+            Employee<int> list2 = new Employee<int>();  //Instantiates 
+            list2.Things = new List<int>();  //Creates new list
+            list2.Things.Add(97865);
+            list2.Things.Add(1556);
+            list2.Things.Add(345);
+
+            for (int i = 0; i < list1.Things.Count ; i++)  //Loop to iterate through lists and display results
+            {
+                Console.WriteLine(list1.Things[i].ToString() + ", " + list2.Things[i].ToString());
+            }
 
             Console.ReadLine();
         }
