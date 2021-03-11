f820c332c36c6bee256e0496ae03428b9d37f8d6
Derek Threeton <derek3ton@hotmail.com>
2021-03-11 15:28:45 -0800

    Add files via upload

---
+++ StructAssignmentPg331/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignmentPg331
+{
+    class Program
+    {
+        public struct Number //Creates struct
+        {
+            public decimal Amount { get; set; }  //Sets Amount as property of struct Number
+        }
+
+
+        static void Main(string[] args)
+        {
+            Number number = new Number(); //Instantiates Number
+            number.Amount = 120;  //Sets value 
+
+            Console.WriteLine(number.Amount); //Outputs the value to console
+
+            Console.ReadLine();
+        }
+    }
+}
