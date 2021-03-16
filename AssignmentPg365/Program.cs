8ecfc4d29bdc21d6650a531a1894b206ef74051c
Derek Threeton <derek3ton@hotmail.com>
2021-03-16 13:19:32 -0700

    Add files via upload

---
+++ AssignmentPg365/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AssignmentPg365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number.");
+            string userInput = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\derek\source\repos\AssignmentPg365\Log.txt", userInput);
+
+
+            string readBack = File.ReadAllText(@"C:\Users\derek\source\repos\AssignmentPg365\Log.txt");
+            userInput = readBack;
+            Console.WriteLine("You chose the number: " + readBack);
+
+            Console.ReadLine();
+        }
+    }
+}
+            
