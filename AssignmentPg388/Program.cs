5be5d8dac86673ad19f73d4d917b5fe89c2300d8
Derek Threeton <derek3ton@hotmail.com>
2021-03-17 17:02:06 -0700

    Add files via upload

---
+++ AssignmentPg388/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg388
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your current age.");
+            int age = Convert.ToInt32(Console.ReadLine());
+            var currentYear = DateTime.Now.Year;
+            
+            if (age <= 0)
+            {
+                try
+                {
+                    var yearBorn = currentYear - age;
+                    Console.WriteLine("Your birth year is: {0}", yearBorn);
+                    Console.ReadLine();
+                    return;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ooops");
+                }
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
