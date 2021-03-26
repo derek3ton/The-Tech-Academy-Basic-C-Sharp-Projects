ba6fc3ce572fdf9f63142a7d78660b1be2f1b591
Derek Threeton <derek3ton@hotmail.com>
2021-03-25 17:58:40 -0700

    update

---
+++ CodeFirstChallenge/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
